79d5bc6d18945304087ba48f3909668f3a147cbc
Belal <BelalGit@gmail.com>
2025-09-22 00:15:13 +0300

    Configs (belal)

---
+++ Restaurant.DAL/Data/Configurations/CategoryConfig.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Restaurant.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.DAL.Data.Configurations
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // Primary Key
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).UseIdentityColumn();
+                
+
+            // Base Entity Properties
+            builder.Property(c => c.CreatedBy)
+                .IsRequired();
+
+            builder.Property(c => c.CreatedOn)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(c => c.ModifiedBy)
+                .IsRequired();
+
+            builder.Property(c => c.ModifiedOn)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(c => c.IsDeleted)
+                .HasDefaultValue(false);
+
+
+            // Category Specific Properties
+            builder.Property(c => c.CategoryName)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("nvarchar(100)");
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(500)
+                .HasColumnType("nvarchar(500)");
+
+            builder.Property(c => c.DisplayOrder)
+                .IsRequired()
+                .HasDefaultValue(0);
+
+            builder.Property(c => c.IsActive)
+                .IsRequired()
+                .HasDefaultValue(true);
+
+
+
+            // Relationships
+            builder.HasMany(c => c.MenuItems)
+                .WithOne(m => m.Category)
+                .HasForeignKey(m => m.CategoryId)
+                .OnDelete(DeleteBehavior.NoAction);
+               
+        }
+    }
+}
---
+++ Restaurant.DAL/Data/Configurations/MenuItemConfig.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Restaurant.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.DAL.Data.Configurations
+{
+    public class MenuItemConfig : IEntityTypeConfiguration<MenuItem>
+    {
+        public void Configure(EntityTypeBuilder<MenuItem> builder)
+        {
+            // Primary Key
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Id).UseIdentityColumn();
+                
+
+            // Base Entity Properties
+            builder.Property(m => m.CreatedBy)
+                .IsRequired();
+
+            builder.Property(m => m.CreatedOn)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(m => m.ModifiedBy)
+                .IsRequired();
+
+            builder.Property(m => m.ModifiedOn)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(m => m.IsDeleted)
+                .HasDefaultValue(false);
+
+
+            // MenuItem Specific Properties
+            builder.Property(m => m.ItemName)
+                .IsRequired()
+                .HasMaxLength(150)
+                .HasColumnType("nvarchar(150)");
+
+            builder.Property(m => m.Description)
+                .HasMaxLength(1000)
+                .HasColumnType("nvarchar(1000)");
+
+            builder.Property(m => m.Price)
+                .IsRequired()
+                .HasColumnType("decimal(10,2)")
+                .HasPrecision(10, 2);
+
+            builder.Property(m => m.ImageUrl)
+                .HasMaxLength(255)
+                .HasColumnType("nvarchar(255)");
+
+            builder.Property(m => m.IsAvailable)
+                .IsRequired()
+                .HasDefaultValue(true);
+
+            builder.Property(m => m.CategoryId)
+                .IsRequired();
+
+
+
+            // Relationships
+            builder.HasOne(m => m.Category)
+                .WithMany(c => c.MenuItems)
+                .HasForeignKey(m => m.CategoryId)
+                .OnDelete(DeleteBehavior.NoAction);
+                
+        }
+    }
+}
