3024d52e05a6c61863446baeac1e0653916cffdf
ahmed elsaadany <ahmed.s.elsaadany@gmail.com>
2025-09-22 20:48:20 +0300

    ...

---
+++ Restaurant.BLL/Services/Classes/TableServices.cs
@@ -0,0 +1,13 @@
+﻿using Restaurant.DAL.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.BLL.Services.Classes
+{
+    public class TableServices
+    {
+    }
+}
